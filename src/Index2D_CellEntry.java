c2d6cb5b89e370edc41b75abb01676d45ded679d
tehila <terantinuo@gmail.com>
2025-01-08 21:49:20 +0200

    a

--- src/Index2D_CellEntry.java
+++ src/Index2D_CellEntry.java
@@ -1,27 +1,73 @@
-// Add your documentation below:
-
-
 public class Index2D_CellEntry implements Index2D {
-    private int x, y;
-    private int width, height; // נוסיף לשם בדיקת תקינות
+    private final String index; // המחרוזת שמייצגת את האינדקס
 
-    public Index2D_CellEntry(int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+    public Index2D_CellEntry(String index) {
+        this.index = index;
     }
 
+    /**
+     * מחזיר את המחרוזת שמייצגת את האינדקס.
+     *
+     * @return ייצוג האינדקס בפורמט "XY".
+     */
+    @Override
+    public String toString() {
+        return index;
+    }
+
+    /**
+     * בודק אם המחרוזת בפורמט "XY", כאשר:
+     * X היא אות "A-Z" או "a-z",
+     * Y הוא מספר שלם בטווח [0-99].
+     *
+     * @return true אם האינדקס תקף, אחרת false.
+     */
     @Override
     public boolean isValid() {
-        return (x >= 0 && x < width) && (y >= 0 && y < height);
+        if (index == null || index.length() < 2) {   // אם האינדקס ריק, או אם האורך שלו קטן 2(1)
+            return false;
+        }
+
+        // בדיקת האות הראשונה
+        String letter = index.substring(0, 1).toUpperCase();    //
+        boolean isLetterValid = java.util.Arrays.asList(Ex2Utils.ABC).contains(letter);
+        if (!isLetterValid) {
+            return false;
+        }
+
+        // בדיקת המספר שאחרי האות
+        try {
+            int number = Integer.parseInt(index.substring(1));
+            return number >= 0 && number <= 99;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
+    /**
+     * מחזיר את ערך ה-X לפי האות הראשונה (מיקום ברשימה Ex2Utils.ABC).
+     *
+     * @return ערך ה-X (מספר שלם).
+     */
     @Override
-    public int getX() { return x; }
+    public int getX() {                                               // האות שלנו
+        if (!isValid()) {                                              //בודקים אם זה בפורמט הנכון של התאים
+            throw new IllegalStateException("Invalid index format");
+        }
+        String letter = index.substring(0, 1).toUpperCase();
+        return java.util.Arrays.asList(Ex2Utils.ABC).indexOf(letter);
+    }
 
+    /**
+     * מחזיר את ערך ה-Y לפי המספר שאחרי האות.
+     *
+     * @return ערך ה-Y (מספר שלם).
+     */
     @Override
-    public int getY() { return y; }
+    public int getY() {
+        if (!isValid()) {
+            throw new IllegalStateException("ERR");
+        }
+        return Integer.parseInt(index.substring(1));
+    }
 }
-
-
--- src/Sheet_Ex2Sheet.java
+++ src/Sheet_Ex2Sheet.java
@@ -1,121 +1,113 @@
 import java.io.IOException;
 // Add your documentation below:
-public class Sheet_Ex2Sheet {
 
-private Cell[][] table;
+public class Sheet_Ex2Sheet implements Sheet {
+    private Cell[][] table;
+    // Add your code here
 
+    // ///////////////////
     public Sheet_Ex2Sheet(int x, int y) {
-         table = new Cell[x][y];
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                table[i][j] = new Cell() {
-                    /**
-                     * Return the input text (aka String) this cell was init by (without any computation).
-                     *
-                     * @return
-                     */
-                    @Override
-                    public String getData() {
-                        return "";
-                    }
-
-                    /**
-                     * Changes the underline string of this cell
-                     *
-                     * @param s
-                     */
-                    @Override
-                    public void setData(String s) {
-
-                    }
-
-                    /**
-                     * Returns the type of this cell {TEXT,NUMBER, FORM, ERR_CYCLE_FORM, ERR_WRONG_FORM}
-                     *
-                     * @return an int value (as defined in Ex2Utils)
-                     */
-                    @Override
-                    public int getType() {
-                        return 0;
-                    }
-
-                    /**
-                     * Changes the type of this Cell {TEXT,NUMBER, FORM, ERR_CYCLE_FORM, ERR_WRONG_FORM}
-                     *
-                     * @param t an int type value as defines in Ex2Utils.
-                     */
-                    @Override
-                    public void setType(int t) {
-
-                    }
-
-                    /**
-                     * Computes the natural order of this entry (cell) in case of a number or a String =0, else 1+ the max of all dependent cells.
-                     *
-                     * @return an integer representing the "number of rounds" needed to compute this cell (using an iterative approach)..
-                     */
-                    @Override
-                    public int getOrder() {
-                        return 0;
-                    }
-
-                    /**
-                     * Changes the order of this Cell
-                     *
-                     * @param t
-                     */
-                    @Override
-                    public void setOrder(int t) {
-
-                    }
-                }; // תא ריק
+        table = new Cell_SCell[x][y];
+        for(int i=0;i<x;i=i+1) {
+            for(int j=0;j<y;j=j+1) {
+                table[i][j] = new Cell_SCell(Ex2Utils.EMPTY_CELL);
             }
         }
+        eval();
+    }
+    public Sheet_Ex2Sheet() {
+        this(Ex2Utils.WIDTH, Ex2Utils.HEIGHT);
     }
 
+    @Override
+    public String value(int x, int y) {
+        String ans = Ex2Utils.EMPTY_CELL;
+        // Add your code here
+
+        Cell c = get(x,y);
+        if(c!=null) {ans = c.toString();}
+
+        /////////////////////
+        return ans;
+    }
+
+    @Override
     public Cell get(int x, int y) {
         return table[x][y];
     }
 
-    public void set(int x, int y, Cell c) {
-        table[x][y] = c;
+    @Override
+    public Cell get(String cords) {
+        Cell ans = null;
+        // Add your code here
+
+        /////////////////////
+        return ans;
     }
 
+    @Override
     public int width() {
         return table.length;
     }
-
+    @Override
     public int height() {
         return table[0].length;
     }
+    @Override
+    public void set(int x, int y, String s) {
+        Cell c = new Cell_SCell(s);
+        table[x][y] = c;
+        // Add your code here
 
-    public int xCell(String c) {
-        // המרת עמודה באותיות למספר
-        return -1; // החזירי ערך דיפולטי זמני
+        /////////////////////
+    }
+    @Override
+    public void eval() {
+        int[][] dd = depth();
+        // Add your code here
+
+        // ///////////////////
     }
 
-    public int yCell(String c) {
-        // המרת שורה ממחרוזת למספר
-        return -1; // החזירי ערך דיפולטי זמני
+    @Override
+    public boolean isIn(int xx, int yy) {
+        boolean ans = xx>=0 && yy>=0;
+        // Add your code here
+
+        /////////////////////
+        return ans;
     }
 
-    public String eval(int x, int y) {
-        return table[x][y].getData(); // שימוש ב-getData במקום getContent
-    }
-
-    public String[][] evalAll() {
-        String[][] result = new String[width()][height()];
-        for (int i = 0; i < width(); i++) {
-            for (int j = 0; j < height(); j++) {
-                result[i][j] = eval(i, j);
-            }
-        }
-        return result;
-    }
-
+    @Override
     public int[][] depth() {
-        int[][] result = new int[width()][height()];
-        // חישוב עומק עבור כל תא
-        return result;
+        int[][] ans = new int[width()][height()];
+        // Add your code here
+
+        // ///////////////////
+        return ans;
+    }
+
+    @Override
+    public void load(String fileName) throws IOException {
+        // Add your code here
+
+        /////////////////////
+    }
+
+    @Override
+    public void save(String fileName) throws IOException {
+        // Add your code here
+
+        /////////////////////
+    }
+
+    @Override
+    public String eval(int x, int y) {
+        String ans = null;
+        if(get(x,y)!=null) {ans = get(x,y).toString();}
+        // Add your code here
+
+        /////////////////////
+        return ans;
     }
 }
